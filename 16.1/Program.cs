40cfc2fecbc182e85dac9b7c2b4faab7c7f67f8c
Lesha07 <simchuklesha@gmail.com>
2024-12-07 17:45:51 +0200

    !

--- 16.1/Program.cs
+++ 16.1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            
+            string sourcePath = Console.ReadLine();
+
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Помилка: Вихідний файл не знайдено.");
+                return;
+            }
+            
+            Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+            string destinationPath = Console.ReadLine();
+         
+            File.Copy(sourcePath, destinationPath, overwrite: true);
+
+            Console.WriteLine("Файл успішно скопійовано!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Сталася помилка: {ex.Message}");
+        }
+    }
+}
